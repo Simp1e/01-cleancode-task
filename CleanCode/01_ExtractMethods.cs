b3366053bcaee867abc98a6431139439471fb631
Simp1e <kolobke@gmail.com>
2014-11-28 12:39:02 +0400

    Decomposition
    Made code more readable

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace CleanCode
 {
-	public static class RefactorMethod
-	{
-		private static void SaveData(string s, byte[] d)
-		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
+    public static class RefactorMethod
+    {
+        private static void SaveData(string s, byte[] d)
+        {
+            WriteFile(s, d);
+            SaveTime(s, d);
+        }
 
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
+        private static void WriteFile(string s, byte[] d)
+        {
+            var fs = new FileStream(s, FileMode.OpenOrCreate);
+            fs.Write(d, 0, d.Length);
 
-			// close files
-			fs1.Close();
-			fs2.Close();
+            fs = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
+            fs.Write(d, 0, d.Length);
+        }
 
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
-		}
-	}
+        private static void SaveTime(string s, byte[] d)
+        {
+            string tf = s + ".time";
+            var fs = new FileStream(tf, FileMode.OpenOrCreate);
+            var t = BitConverter.GetBytes(DateTime.Now.Ticks);
+            fs.Write(t, 0, t.Length);
+            fs.Close();
+        }
+    }
 }
